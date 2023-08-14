cd88c8c9c9fbb2b2c74889d03674b090d10bc62b
samarthkalashetty <samarthkalashetti2612@gmail.com>
2023-08-14 13:23:58 +0530

    Welcome to HashTable

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hashtable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to HashTable");
+        }
+    }
+}
